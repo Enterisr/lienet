15e4f2900b882f911e620a02dbe17e5f9456fe77
drunkTolstoy <drunktolstoy@gmail.com>
2019-12-29 21:36:17 +0200

    fffffuck

--- client/src/routes/Artic.svelte
+++ client/src/routes/Artic.svelte
@@ -2,29 +2,32 @@
   import { push, location } from "svelte-spa-router";
   $: display = "default display";
   $: title = "loading...";
-  console.log($location);
-  let article = $location.slice(1);
-  if (!isNaN(article)) {
-    let originAddr = document.location.origin;
-    if (window.location.href.includes("localhost")) {
-      const uri = new URL(window.location.href);
-      uri.port = "6969";
-      urlAddr = uri;
-      originAddr = urlAddr.origin;
+  async function GetArticle() {
+    let article = $location.slice(1);
+    if (!isNaN(article)) {
+      let originAddr = document.location.origin;
+      if (window.location.href.includes("localhost")) {
+        const uri = new URL(window.location.href);
+        uri.port = "6969";
+
+        originAddr = uri.origin;
+      }
+      return fetch(originAddr + "/connect?num=" + article)
+        .then(dat => {
+          console.log(dat);
+          return dat.json();
+        })
+        .then(dat => {
+          display = dat.text;
+          title = dat.title;
+          return dat;
+        })
+        .catch(err => {
+          alert(err);
+        });
     }
-    fetch(originAddr + "/connect?num=" + article)
-      .then(dat => {
-        console.log(dat);
-        return dat.json();
-      })
-      .then(dat => {
-        display = dat.text;
-        title = dat.title;
-      })
-      .catch(err => {
-        alert(err);
-      });
   }
+
   export let title;
 </script>
 
@@ -61,6 +64,38 @@
       padding: 0;
     }
   }
+  .lds-ripple {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-ripple div {
+    position: absolute;
+    border: 4px solid #fff;
+    opacity: 1;
+    border-radius: 50%;
+    animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  }
+  .lds-ripple div:nth-child(2) {
+    animation-delay: -0.5s;
+  }
+  @keyframes lds-ripple {
+    0% {
+      top: 36px;
+      left: 36px;
+      width: 0;
+      height: 0;
+      opacity: 1;
+    }
+    100% {
+      top: 0px;
+      left: 0px;
+      width: 72px;
+      height: 72px;
+      opacity: 0;
+    }
+  }
 </style>
 
 <div>
@@ -74,5 +109,14 @@
   <h2 class="title-div">{title}</h2>
 </div>
 <article>
-  <div class="article-body">{display}</div>
+  {#await GetArticle()}
+    <div class="lds-ripple">
+      <div />
+      <div />
+    </div>
+  {:then display}
+    <div class="article-body">{display.text}</div>
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
 </article>
