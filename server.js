075fd8f8bc118449a86c20f1bcd38211a108ac26
Enterisr <1948or1967@gmail.com>
2020-02-06 13:51:54 +0200

    lets try again, like we did last summer

--- server.js
+++ server.js
@@ -187,14 +187,19 @@ app.get('/adminDetails', utils.ensureToken, (req, res, next) => {
 	});
 });
 app.get('/admin', utils.ensureToken, (req, res, next) => {
-	app.use(express.static(path.join(__dirname, 'admin/public')));
+	app.use("/admin",express.static(path.join(__dirname, 'client/public')));
+app.use("/admin",express.static(path.join(__dirname, 'client/public/build')));
+
 	res.sendFile(path.resolve('admin', 'public', 'index.html'));
 });
 
 app.get('/', (req, res) => {
 	res.sendFile(path.resolve('client', 'public', 'index.html'));
 });
-app.use('/admin', express.static(path.join(__dirname, 'admin/public')));
+app.use("/admin",express.static(path.join(__dirname, 'admin/public')));
+app.use("/admin",express.static(path.join(__dirname, 'admin/public/build')));
+
 app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.static(path.join(__dirname, 'client/public/build')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
